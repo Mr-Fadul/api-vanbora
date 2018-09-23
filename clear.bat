3c4f3e90dd7d5d5106ff8395f589530d224657b5
Daniel Silva <danielrodriguesdrs331@gmail.com>
2018-09-23 02:21:08 -0300

    [new] - translate of datatables

---
+++ clear.bat
@@ -0,0 +1,2 @@
+rmdir c:\Users\danie\Desktop\vanbora_api\app\cache\  /S /Q
+
--- src/AppBundle/Resources/public/backend/js/scripts.js
+++ src/AppBundle/Resources/public/backend/js/scripts.js
@@ -2188,15 +2188,44 @@ $(document).ready(function() {
 
 	function qp_datatables(){
 
-		// All datatables must have the class ".datatables" added to their table tag
-		var placeholder = '.table-datatable';
+        // All datatables must have the class ".datatables" added to their table tag
+        var placeholder = '.table-datatable';
 
-		if($(placeholder).length){
-			$(placeholder).each(function(){
-				$(this).DataTable();
-			});
-		}
-	}
+        if($(placeholder).length){
+            $(placeholder).each(function(){
+                $(this).DataTable({
+                    searching: true,
+                    ordering: true,
+                      select: true,
+                      "lengthMenu": [[20, 50, 100, -1], [20, 50, 100, "Todos"]],
+                    "language": {
+                        "sEmptyTable": "Nenhum registro encontrado",
+                        "sInfo": "Mostrando de _START_ até _END_ de _TOTAL_ registros",
+                        "sInfoEmpty": "Mostrando 0 até 0 de 0 registros",
+                        "sInfoFiltered": "(Filtrados de _MAX_ registros)",
+                        "sInfoPostFix": "",
+                        "sInfoThousands": ".",
+                        "sLengthMenu": "Mostrar _MENU_ resultados por página",
+                        "sLoadingRecords": "Carregando...",
+                        "sProcessing": "Processando...",
+                        "sZeroRecords": "Nenhum registro encontrado",
+                        "sSearch": "Pesquisar..",
+                        "oPaginate": {
+                            "sNext": "Próximo",
+                            "sPrevious": "Anterior",
+                            "sFirst": "Primeiro",
+                            "sLast": "Último"
+                        },
+                        "oAria": {
+                            "sSortAscending": ": Ordenar colunas de forma ascendente",
+                            "sSortDescending": ": Ordenar colunas de forma descendente"
+                        }
+                    }
+                }
+                );
+            });
+        }
+    }
 
 	function qp_form_wizard(){
 		var thisForm = '#rootwizard-1';
